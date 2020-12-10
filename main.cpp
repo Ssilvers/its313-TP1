f29ded152b47c69f34ef7cb7ff7c45f2d019644e
Antonin <antonin.deschamps71@gmail.com>
2020-12-10 10:43:59 +0100

    Modif pour tableau chambres

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 
 #include "date.h"
@@ -9,16 +10,8 @@
 using namespace date;
 using std::cout;
 using std::endl;
-/*std::ostream& operator<<(std::ostream& os, const std::vector<Livre> livres) {
-    auto i=livres.begin();
-	while(i!=livres.end()) {       
-		os << *i << "|";
-		++i;
-	}
-os << endl;
-return os;
-}
-*/
+
+
 
 int main(){
 
@@ -54,9 +47,9 @@ cout << Calif.infoHotel() << endl;
 Calif.modifier(0,"Bellagio","Las Vegas");
 cout << Calif.infoHotel() << endl;
 
-Hotel.addChambre(A);
-cout << "Chambres du" << Calif.getNom() << ":" << endl;
-cout << Calif.chambres()<< endl;
+Calif.addChambre(A);
+cout << "Chambres du " << Calif.getNom() << ":" << endl;
+Calif.chambres();
 
 return 0;
 }
